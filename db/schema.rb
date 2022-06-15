f770304c954be054f085e38b585c36f5a1b7f86f
Kazuya-Sakashita <kz0508@gmail.com>
2022-06-16 07:34:50 +0900

    名前ログイン

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_220843) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
